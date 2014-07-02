0818a2367a5b2fc1b93365e42bc8754a59fd79e4
Juan Fco <slipkacho21metal@hotmail.com>
2014-07-02 18:59:43 -0500

    Propagación de datos

--- Spring Jdbc - Propagacion de datos/spring-jdbc-01/src/test/java/test/TestPersonaServiceImpl.java
+++ Spring Jdbc - Propagacion de datos/spring-jdbc-01/src/test/java/test/TestPersonaServiceImpl.java
@@ -30,29 +30,79 @@ public class TestPersonaServiceImpl {
 	private PersonaDao personaDao;
 
 	@Test
-	@Transactional   //cuando se hacen pruebas unitarias es comun usar esta anotacion para evitar que haga algun commit, haciendo un rollback y no altera la tablas
-	public void deberiaMostrarPersonas(){
-		try{
+	// @Transactional //cuando se hacen pruebas unitarias es comun usar esta
+	// anotacion para evitar que haga algun commit, haciendo un rollback y no
+	// altera la tablas
+	public void deberiaMostrarPersonas() {
+		try {
 			System.out.println();
 			logger.info("Inicio del test");
 			int contadorPersonas = this.desplegarPersonas();
-			
-			//segun el no. de personas recuperas, deberia ser el mismo de la tabla
+
+			// segun el no. de personas recuperas, deberia ser el mismo de la
+			// tabla
 			assertEquals(contadorPersonas, personaDao.contadorPersonas());
 			logger.info("Fin del test");
-		}catch (Exception e){
+		} catch (Exception e) {
 			logger.info("Error Servico ", e);
 		}
 	}
-	
-	private int desplegarPersonas(){
+
+	private int desplegarPersonas() {
 		List<Persona> personas = personaService.listarPersonas();
 		int contadorPersonas = 0;
-		for (Persona persona : personas){
-			logger.info("Personas: "+ persona);
-			contadorPersonas ++;
+		for (Persona persona : personas) {
+			logger.info("Personas: " + persona);
+			contadorPersonas++;
 		}
 		return contadorPersonas;
-		
+
+	}
+
+	@Test
+	@Transactional// relizaremos un rollback
+	public void testOperaciones() {
+		try {
+			System.out.println();
+			logger.info("Iniciando testOperaciones");
+			Persona persona1 = new Persona();
+			persona1.setNombre("Andrea");
+			//persona1.setNombre("Andrea111111111111111111111111111111111111111111111111111111111111111111111");
+			persona1.setApePaterno("Lara");
+			persona1.setEmail("dolgasanz@email.com");
+			personaService.agregarPersona(persona1);
+
+			// Deberiamos tener 4 personas
+			assertEquals(4, personaDao.contadorPersonas());
+
+			// Actualizamos la perosna con id=1
+			Persona persona2 = personaService.recuperarPersona(new Persona(1));
+			persona2.setNombre("Administrador");
+			personaService.modificarPersona(persona2);
+
+			this.desplegarPersonas();
+
+			logger.info("Fin del testOperaciones");
+			System.out.println();
+
+		} catch (Exception e) {
+			logger.info("Error testOperaciones", e);
+		}
+	}
+	
+	@Test
+	public void compruebaOperaciones(){
+		try{
+			logger.info("Iniciando compruebaOperaciones");
+			//deberiamos tener 3 personas ya que se hizo rollback antes
+			assertEquals(3, personaDao.contadorPersonas());
+			
+			this.desplegarPersonas();
+			
+			logger.info("Fin compruebaOperaciones");
+			
+		}catch(Exception e){
+			logger.info("Error Comprueba operaciones ", e);
+		}
 	}
 }
