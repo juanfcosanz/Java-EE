f74c51537ed6d2d4323c325faf9b3a2525ac89a7
Juan Fco <slipkacho21metal@hotmail.com>
2014-07-13 18:49:05 -0500

    Manejo de eventos ValueChangeListener en JSF
    Facets 2.0

---
+++ BolsaTrabajoV5/src/bean/backing/VacanteForm.java
@@ -0,0 +1,77 @@
+package bean.backing;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+import bean.model.Candidato;
+
+@ManagedBean
+@RequestScoped
+public class VacanteForm {
+
+	public VacanteForm() {
+	}
+
+	@ManagedProperty(value = "#{candidato}")
+	private Candidato candidato;
+
+	public Candidato getCandidato() {
+		return candidato;
+	}
+
+	public void setCandidato(Candidato candidato) {
+		this.candidato = candidato;
+	}
+
+	public String enviar() {
+		System.out.println("enviar() Nombre= " + this.candidato.getNombre());
+		System.out
+				.println("enviar() Apellido= " + this.candidato.getApellido());
+		System.out.println("enviar() sueldoDeseado= "
+				+ this.candidato.getSueldoDeseado());
+		System.out.println("enviar() Nacimiento: "
+				+ this.candidato.getFechaNacimiento());
+
+		if (this.candidato.getNombre().equals("Juan")) {
+			if (this.candidato.getApellido().equals("Perez")) {
+				String msg = "Gracias, pero Juan Perez ya trabaja con nosotros.";
+				FacesMessage facesMessage = new FacesMessage(
+						FacesMessage.SEVERITY_ERROR, msg, msg);
+				FacesContext facesContext = FacesContext.getCurrentInstance();
+				String clienteId = null; // Este es un mensaje global
+				facesContext.addMessage(clienteId, facesMessage);
+				return "index";
+			}
+			return "exito";
+		} else {
+			return "fallo";
+		}
+
+	}
+
+	public void codigoPostalListener(ValueChangeEvent valueChangeEvent) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		UIViewRoot uiViewRoot = facesContext.getViewRoot();
+		String newCodigoPortal = (String) valueChangeEvent.getNewValue();
+		
+		if("03810".equals(newCodigoPortal)){
+			UIInput ciudadInputText = (UIInput) uiViewRoot.findComponent("vacanteForm:ciudad");
+			String ciudad = "DF";
+			ciudadInputText.setValue(ciudad);
+			ciudadInputText.setSubmittedValue(ciudad);
+			
+			UIInput coloniaInputText = (UIInput) uiViewRoot.findComponent("vacanteForm:colonia");
+			String colonia = "Nápoles";
+			coloniaInputText.setValue(colonia);
+			coloniaInputText.setSubmittedValue(colonia);
+			
+			facesContext.renderResponse();
+		}
+	}
+}
---
+++ BolsaTrabajoV5/src/bean/model/Candidato.java
@@ -0,0 +1,83 @@
+package bean.model;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Candidato {
+
+	public Candidato() {
+
+	}
+
+	private String nombre = "Introduce Nombre";
+	private String apellido = "Introduce Apellidos";
+	private int sueldoDeseado;
+	private Date fechaNacimiento;
+	private String ciudad;
+	private String colonia;
+	private String codigoPostal;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getSueldoDeseado() {
+		return sueldoDeseado;
+	}
+
+	public void setSueldoDeseado(int sueldoDeseado) {
+
+		System.out.println("Modificacion del modelo. Valor sueldo: "
+				+ sueldoDeseado);
+		this.sueldoDeseado = sueldoDeseado;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getColonia() {
+		return colonia;
+	}
+
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+}
